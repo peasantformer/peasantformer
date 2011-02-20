fd9bb6dd023f515fe40f295d565b0f3bbcc8b108
Alexander Tumin <itakingiteasy@gmail.com>
2011-02-20 13:17:37 +0300

    ok

--- phys/src/phys.cpp
+++ phys/src/phys.cpp
@@ -1,5 +1,5 @@
 #include <math.h>
-#include <vector>
+#include <list>
 #include <string.h>
 #include <stdlib.h>
 #include "SDL/SDL.h"
@@ -78,11 +78,56 @@ inline Vector2 operator-(Vector2 l, Vector2 r) {
 	return Vector2(l.x-r.x, l.y-r.y);
 }
 
+template <class T>
+class Array {
+	private:
+		int size;
+		int alloc;
+		T *data;
+	public:
+		Array() {
+			this->size = 0;
+			this->alloc = 0;
+			this->data = NULL;
+		}
+		~Array() {
+			this->size = 0;
+			this->alloc = 0;
+			free(this->data);
+		}
+	public:
+		T operator[](int i) {
+			return this->data[i];
+		}
+	public:
+		void add_item(T item) {
+			this->size++;
+			if (this->size > this->alloc) {
+				this->alloc++;
+				this->data = (T*)realloc(this->data,sizeof(T));
+			}
+			this->data[this->size-1] = item;
+		}
+		void del_item (int i) {
+			if (this->size == 0) return;
+			this->count--;
+			if (this->count == i) return;
+			this->data[i] = this->data[this->count];
+		}
+		void clear() {
+			this->size = 0;
+			this->alloc = 0;
+			free(this->data);
+			this->data = NULL;	
+		}
+};
 
 struct RevSectIndex {
 	int section;
 	int index;
 };
+
+
 
 class Particle {
 	public:
@@ -94,9 +139,7 @@ class Particle {
 		float bounceness;
 		bool pinned;
 		
-		RevSectIndex *rev_sections;
-		int rev_count;
-		int rev_alloc;
+		Array<RevSectIndex> rev_sections;
 	public:
 		Particle() {
 			this->position = Vector2(0,0);
@@ -106,9 +149,7 @@ class Particle {
 			this->inv_mass = 1;
 			this->bounceness = 1;
 			this->pinned = false;
-			this->rev_sections = NULL;
-			this->rev_count = 0;
-			this->rev_alloc = 0;
+
 		}
 		Particle(Vector2 p, Vector2 s, float w, float h, float m, float b, bool pinned) {
 			this->position = p;
@@ -118,40 +159,21 @@ class Particle {
 			this->inv_mass = m;
 			this->bounceness = b;
 			this->pinned = pinned;
-			this->rev_sections = NULL;
-			this->rev_count = 0;
-			this->rev_alloc = 0;
+
 		}
+
 	public:
 		void add_rev(int section, int index) {
-			this->rev_count++;
-			
-			if (this->rev_count > this->rev_alloc) {
-				this->rev_alloc++;
-				this->rev_sections = (RevSectIndex *)realloc(this->rev_sections, this->rev_alloc * sizeof(RevSectIndex));
-			}
 			RevSectIndex rsi;
 			rsi.section = section;
 			rsi.index = index;
-			this->rev_sections[this->rev_count-1] = rsi;
+			this->rev_sections.add_item(rsi);
 		}
-		void del_rev(int n) {
-			if (this->rev_count == 0) return;
-			int i=0;
-			
-			for (int z=0; z < this->rev_count; z++) {
-				if (this->rev_sections[z].section == n) {
-					i = z;
-					break;
-				}
-			}
-			this->rev_count--;
-			if (this->rev_count == i) return;
-			this->rev_sections[i] = this->rev_sections[this->rev_count];
+		
+		void clear_rev() {
+			this->rev_sections.clear();
 		}
-		int size_rev() {
-			return this->rev_count;
-		}
+		
 		void draw() {
 			if (position.x < 0 || position.y < 0) return;
 		}
@@ -160,122 +182,22 @@ class Particle {
 
 class Section {
 	private:
-		int count;
-		int allocated;
-		Particle **members;
+		std::list<Particle *> members;
 	public:
-		int x;
-		int y;
+		int x,y;
 	public:
 		Section(int x, int y) {
 			this->x = x;
 			this->y = y;
-			
-			this->count = 0;
-			this->allocated = 0;
-			this->members = NULL;
-		}
-		~Section() {
-			free(this->members);
-			this->members = NULL;
 		}
 	public:
 		Particle *operator[](int i) {
-			return this->members[i];
-		}
-	public:
-		int add(Particle *particle) {
-			this->count++;
-			if (this->count > this->allocated) {
-				this->allocated++;
-				this->members = (Particle **)realloc(this->members, this->allocated * sizeof(Particle *));
-			}
-
-			this->members[this->count-1] = particle;
-			return this->count-1;
-		}
-		void del(int i) {
-			if (this->count == 0) return;
-			this->count--;
-			if (this->count == i) return;
-			this->members[i] = this->members[this->count];
-		}
-		int size() {
-			return this->count;
+			std::list<Particle *>::iterator it;
+			it = this->members.begin();
+			return *it;
 		}
 		
 };
-
-
-class Level {
-	private:
-		int sec_count;
-		Section *sections;
-		
-		int pt_count;
-		int pt_alloc;
-		Particle *particles;
-	public:
-		int x,y;
-		int width,height;
-		int section_width;
-		int section_height;
-	public:
-		Level(int x, int y, int un_width, int un_height, int s_w, int s_h) {
-			this->x = x;
-			this->y = y;
-			this->width = un_width/s_w;
-			this->height = un_height/s_h;
-			if (this->width == 0) this->width = 1;
-			if (this->height == 0) this->height = 1;
-			this->section_width = s_w;
-			this->section_height = s_h;
-			this->sec_count = this->width * this->height;
-			this->sections = (Section *)malloc(this->sec_count * sizeof(Section));
-			this->pt_count = 0;
-			this->pt_alloc = 0;
-			this->particles = NULL;
-
-			int cnt=0;
-			for (int x=0; x < this->width; x++) {
-				for (int y=0; y < this->height; y++) {
-					this->sections[cnt++] = Section(x,y);
-				}
-			}
-		}
-		~Level() {
-			free(this->sections);
-			free(this->particles);
-			this->sections = NULL;
-			this->particles = NULL;
-		}
-	public:
-		void add_obj(Particle pt) {
-			int x = (int)pt.position.x/(int)section_width;
-			int y = (int)pt.position.y/(int)section_height;
-			
-			if (x >= this->width || y >= this->height) return;
-
-			this->pt_count++;
-
-			if (this->pt_count > this->pt_alloc) {
-				this->pt_alloc++;
-				this->particles = (Particle *)realloc(this->particles, this->pt_alloc * sizeof (Particle));
-			}
-			
-			this->particles[this->pt_count-1] = pt;
-
-			int cnt = height * x + y;
-			int s;
-			
-			
-//			s = this->sections[cnt].add(&this->particles[this->pt_count-1]);
-//c			this->particles[this->pt_count-1].add_rev(cnt,s);
-			
-						
-		}
-};
-
 
 int main(int argc, char **argv) {
 	int level_width = 800;
@@ -283,8 +205,9 @@ int main(int argc, char **argv) {
 	int section_width = 100;
 	int section_height = 100;
 
-	Level lvl(0,0,200,800,100,100);
-	lvl.add_obj(Particle(Vector2(190,190), Vector2(0,0), 10, 10, 1, 1, false));
+//	Level lvl(0,0,200,200,100,100);
+//	lvl.add_obj(Particle(Vector2(150,150), Vector2(0,0), 100, 100, 1, 1, false));
+//	lvl.add_obj(Particle(Vector2(150,150), Vector2(0,0), 100, 100, 1, 1, false));	
 //	s.add(Particle(Vector2(150,150), Vector2(0,0), 10, 10, 1, 1, false));
 /*
 	World world;
