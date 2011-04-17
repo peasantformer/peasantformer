e9017544c7529b47c8d5aa3f15db96cf8228d49d
Alexander Tumin <itakingiteasy@gmail.com>
2011-04-17 19:33:39 +0400

    A time to stop and start all over again

--- src/PhysTest/PhysTest.cpp
+++ src/PhysTest/PhysTest.cpp
@@ -6,13 +6,15 @@ class Point {
 		Vector2f proj_pos;
 		Vector2f speed;
 		float inv_mass;
+		bool was_computed;
 
-		Point() : pos(0,0), proj_pos(0,0), speed(0,0), inv_mass(1) {}
+		Point() : pos(0,0), proj_pos(0,0), speed(0,0), inv_mass(1), was_computed(false) {}
 		Point(Vector2f pos, float inv_mass) :
 			pos(pos),
 			proj_pos(pos),
 			speed(0,0),
-			inv_mass(inv_mass) {}
+			inv_mass(inv_mass),
+			was_computed(false) {}
 
 		void draw() {
 			glLoadIdentity();
@@ -33,16 +35,32 @@ class Joint {
 	public:
 		Point *p1;
 		Point *p2;
+		Point *p3;
 		float stiffness;
-		float length;
+		float length1;
+		float length2;
+		bool upper;
 
-		Joint() : p1(NULL), p2(NULL), stiffness(0), length(0) {}
-		Joint(Point *p1, Point *p2, float length, float stiffness) :
+		Joint() : 
+			p1(NULL), 
+			p2(NULL), 
+			p3(NULL), 
+			stiffness(0), 
+			length1(0), 
+			length2(0),
+			upper(true)
+		{}
+		Joint(Point *p1, Point *p2, Point *p3, float length1, float length2, float stiffness, bool upper) :
 			p1(p1),
 			p2(p2),
+			p3(p3),
 			stiffness(stiffness),
-			length(length) 
-		{}
+			length1(length1),
+			length2(length2),
+			upper(upper)
+		{
+
+		}
 	
 		void draw() {
 			glLoadIdentity();
@@ -51,44 +69,128 @@ class Joint {
 			glBegin(GL_LINES);
 				glVertex2f(p1->pos.x,p1->pos.y);
 				glVertex2f(p2->pos.x,p2->pos.y);
+				glVertex2f(p2->pos.x,p2->pos.y);
+				glVertex2f(p3->pos.x,p3->pos.y);
 			glEnd();
 			glLoadIdentity();
 		}
-		void jointer(Point *rp1, Point *rp2) {
+		Vector2f jointer(Point *rp1, Point *rp2) {
+				/*
 				float cat1 = (rp1->proj_pos.x - rp2->pos.x);
 				float cat2 = (rp1->proj_pos.y - rp2->pos.y);
 				
 				cat1 = -cat1;
 				cat2 = cat2;
 				
-				
-				//if (cat1 < 50)
-				//SDL_Delay(3);
-	
-				//printf("%f %f\n",cat1, cat2);
-				
 				float hyp = sqrt(cat1 * cat1 + cat2 * cat2);
 				
 				
 				float sine = cat2 / hyp; 
 				float cosine = cat1 / hyp;
+				
+				printf("%f %f\n",sine,cosine);
 
 				float res_x, res_y;
 				
 				res_x = (-length) * cosine - 0;
 				res_y = -(-length) * sine + 0;
-
-				//printf("%f %f\n",rp2->pos.x + res_x, rp2->pos.y + res_y);
-
-				rp1->proj_pos = Vector2f(rp2->pos.x + res_x,rp2->pos.y + res_y);
+				
+				return Vector2f(rp2->pos.x + res_x,rp2->pos.y + res_y);
+				*/
+				return Vector2f(0,0);
 		}
 		void apply() {
+			float catf1 = (p1->proj_pos.x - p2->pos.x);
+			float catf2 = (p1->proj_pos.y - p2->pos.y);
+			float cats1 = (p2->proj_pos.x - p1->pos.x);
+			float cats2 = (p2->proj_pos.y - p1->pos.y);
+			
+			float catss1 = (p2->proj_pos.x - p3->pos.x);
+			float catss2 = (p2->proj_pos.y - p3->pos.y);
+			
+			float catt1 = (p3->proj_pos.x - p2->pos.x);
+			float catt2 = (p3->proj_pos.y - p2->pos.y);
+			
+			catf1 = -catf1;
+			cats1 = -cats1;
+			catss1 = -catss1;
+			catt1 = -catt1;
+			
+			float hypf = sqrt(catf1 * catf1 + catf2 * catf2);
+			float hyps = sqrt(cats1 * cats1 + cats2 * cats2);
+			
+			float hypss = sqrt(catss1 * catss1 + catss2 * catss2);
+			
+			float hypt = sqrt(catt1 * catt1 + catt2 * catt2);
+			
+			float sinef = catf2 / hypf; 
+			float cosinef = catf1 / hypf;
+			float sines = cats2 / hyps; 
+			float cosines = cats1 / hyps;
+			
+			float siness = catss2 / hypss; 
+			float cosiness = catss1 / hypss;
+			
+			float sinet = catt2 / hypt; 
+			float cosinet = catt1 / hypt;
+			
+			float res_xf = (-length1) * cosinef - 0;
+			float res_yf = (length1) * sinef + 0;
+			float res_xs = (-length1) * cosines - 0;
+			float res_ys = (length1) * sines + 0;
+			
+			float res_xss = (-length2) * cosiness - 0;
+			float res_yss = (length2) * siness + 0;
+			
+			float res_xt = (-length2) * cosinet - 0;
+			float res_yt = (length2) * sinet + 0;
+			
+			if (p1->proj_pos != p1->pos && p1->was_computed == false) {
+				p1->proj_pos = Vector2f(p2->pos.x + res_xf, p2->pos.y + res_yf);
+				p1->was_computed = true;
+			}
+			
+			if (p2->proj_pos != p2->pos && p2->was_computed == false) {
+				//printf("%f\n",);
+				if (!upper)
+					p2->proj_pos = Vector2f(p3->pos.x + res_xss, p3->pos.y + res_yss);
+				else 
+					p2->proj_pos = Vector2f(p1->pos.x + res_xs, p1->pos.y + res_ys);
+				p2->was_computed = true;
+			}
+			
+			if (p3->proj_pos != p3->pos && p3->was_computed == false) {
+				p3->proj_pos = Vector2f(p2->pos.x + res_xt, p2->pos.y + res_yt);
+				p3->was_computed = true;
+			}
+
+			//if (p2->proj_pos != p2->pos)
+			//p2->proj_pos = Vector2f(p2->pos.x + res_xs, p2->pos.y + res_ys);
+			
+			//p2->proj_pos = Vector2f(p1->pos.x + res_xf,p1->pos.y + res_yf);
+			//p3->proj_pos = Vector2f(p2->pos.x + res_xs,p2->pos.y + res_ys);
+			
+			
+			
+				
+			
+			/*
+			Vector2f fst = p1->proj_pos;
+			Vector2f snd = p2->proj_pos;
+			Vector2f trd = p3->proj_pos;
 			if (p1->pos != p1->proj_pos) {
-				this->jointer(p1,p2);
+				fst = this->jointer(p1,p2);
 			}
 			if (p2->pos != p2->proj_pos) {
-				this->jointer(p2,p1);
+				snd = this->jointer(p2,p3);
 			}
+			if (p3->pos != p3->proj_pos) {
+				trd = this->jointer(p3,p2);
+			}
+			p1->proj_pos = fst;
+			p2->proj_pos = snd;
+			p3->proj_pos = trd;
+			*/
 		}
 	
 };
@@ -123,17 +225,22 @@ class Stick : public Object {
 		Point p1;
 		Point p2;
 		Point p3;
+		
+		Point p4;
+		Point p5;
 		Joint holder;
 		Joint holder2;
 	
 		Stick() : pinned(false) {}
 		Stick(Vector2f pos, float length) :
 			pinned(false),
-			p1(Vector2f(pos.x-length/2,pos.y),1),
-			p2(Vector2f(pos.x+length/2,pos.y),1),
-			p3(Vector2f(pos.x+length*1.5,pos.y),1),
-			holder(&this->p1, &this->p2, length, 0.5) ,
-			holder2(&this->p2, &this->p3, length, 0.5) 
+			p1(Vector2f(pos.x-length,pos.y),1),
+			p2(Vector2f(pos.x,pos.y),1),
+			p3(Vector2f(pos.x+length,pos.y),1),
+			p4(Vector2f(pos.x+length*2,pos.y),1),
+			p5(Vector2f(pos.x+length*3,pos.y),1),
+			holder(&this->p1, &this->p2, &this->p3, length,length, 0.5,false),
+			holder2(&this->p3, &this->p4, &this->p5, length, length, 0.5, true)
 		{
 			this->type = O_STICK;
 		}
@@ -145,21 +252,32 @@ class Stick : public Object {
 			this->p1 = rhs.p1;
 			this->p2 = rhs.p2;
 			this->p3 = rhs.p3;
+			this->p4 = rhs.p4;
+			this->p5 = rhs.p5;
 			this->holder = rhs.holder;
-			this->holder2 = rhs.holder2;
 			this->holder.p1 = &this->p1;
 			this->holder.p2 = &this->p2;
-			this->holder2.p1 = &this->p2;
-			this->holder2.p2 = &this->p3;
+			this->holder.p3 = &this->p3;
+			
+			this->holder2 = rhs.holder2;
+			this->holder2.p1 = &this->p3;
+			this->holder2.p2 = &this->p4;
+			this->holder2.p3 = &this->p5;
 			return *this;
 		}
 	
 		void draw() {
 			this->p1.draw();
+
 			this->p2.draw();
+
 			this->p3.draw();
+			
+			this->p4.draw();
+			this->p5.draw();
+
 			this->holder.draw();
-			this->holder2.draw();	
+			this->holder2.draw();
 			
 			glLoadIdentity();
 			
@@ -176,6 +294,11 @@ class Stick : public Object {
 			glLoadIdentity();
 		}
 		void compute() {
+			this->p1.was_computed = false;
+			this->p2.was_computed = false;
+			this->p3.was_computed = false;
+			this->p4.was_computed = false;
+			this->p5.was_computed = false;
 			this->holder.apply();
 			this->holder2.apply();
 		}
@@ -188,9 +311,11 @@ class Stick : public Object {
 			p->proj_pos = p->pos + p->speed * dt;
 		}
 		void project(Vector2f ext,float dt) {
-			//project_pt(&p1,ext,dt);
+			project_pt(&p1,ext,dt);
 			project_pt(&p2,ext,dt);
 			project_pt(&p3,ext,dt);
+			project_pt(&p4,ext,dt);
+			project_pt(&p5,ext,dt);
 		}
 		void update_pt(Point *p, float dt) {
 			p->speed = (p->proj_pos - p->pos) / dt;
@@ -200,6 +325,8 @@ class Stick : public Object {
 			//update_pt(&p1, dt);
 			update_pt(&p2, dt);
 			update_pt(&p3, dt);
+			update_pt(&p4, dt);
+			update_pt(&p5, dt);
 		}
 };
 
@@ -253,7 +380,7 @@ class World {
 		Vector2f gravity;
 		float dt;
 
-		World() : gravity(0,10), dt(0.001) {}
+		World() : gravity(0,10), dt(0.005) {}
 
 		void add_stick(Vector2f pos, float length) {
 			size_t num;
@@ -351,7 +478,7 @@ int main(int argc, char **argv) {
 	World world;
 	
 	
-	world.add_stick(Vector2f(500,100),100);
+	world.add_stick(Vector2f(200,200),50);
 	world.add_plain(Vector2f(300,600),600,100);
 	
 	//test();
